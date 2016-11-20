d18b487f972625648843671197c17063745ec5b5
ch-andrei <im.andrei@hotmail.ca>
2016-11-20 17:46:11 -0500

    Initial File Hierarchy

---
+++ PixEditor/src/Main.java
@@ -0,0 +1,7 @@
+/**
+ * Created by Andrei-ch on 2016-11-20.
+ */
+public class Main {
+
+
+}
---
+++ PixEditor/src/filters/ConvolutionFilter.java
@@ -0,0 +1,7 @@
+package filters;
+
+/**
+ * Created by Andrei-ch on 2016-11-20.
+ */
+public class ConvolutionFilter {
+}
