c802467d056392555513a236c37036a366a2441f
ch-andrei <im.andrei@hotmail.ca>
2016-11-20 19:00:57 -0500

    Added code to read/write png images

---
+++ PixEditor/src/image_utilities/ImageRW.java
@@ -0,0 +1,29 @@
+package image_utilities;
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+/**
+ * Created by Andrei-ch on 2016-11-20.
+ */
+public class ImageRW {
+
+    public static BufferedImage readImage(String filename){
+        BufferedImage image = null;
+        try {
+            image = ImageIO.read(new File(filename + ".png"));
+        } catch (IOException e) {
+        }
+        return image;
+    }
+
+    public static void saveImage(BufferedImage image, String filename){
+        try {
+            File outputfile = new File(filename + ".png");
+            ImageIO.write(image, "png", outputfile);
+        } catch (IOException e) {
+        }
+    }
+}
