26f6d7039cea3d931143f14f9fa4c76e3c8b4534
ch-andrei <im.andrei@hotmail.ca>
2016-11-21 17:29:18 -0500

    ported from abstract class

---
+++ PixEditor/src/filters/linear_filters/RectificationLinearFilter.java
@@ -0,0 +1,19 @@
+package filters.linear_filters;
+
+import filters.LinearFilter;
+
+/**
+ * Created by Andrei-ch on 2016-11-21.
+ */
+public class RectificationLinearFilter extends LinearFilter{
+    public RectificationLinearFilter(int number_of_threads){
+        this.number_of_threads = number_of_threads;
+    }
+    @Override
+    public int applyFilterOnPixel(int val){
+        val -= 127;
+        val = (val >= 0) ? val : 0;
+        val += 127;
+        return val;
+    }
+}
